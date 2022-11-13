2dbe31d402739d079b0016ad3f530b8e6447fb45
ZoniIC645 <michael20030908@gmail.com>
2022-11-14 01:38:31 +0900

    delete item registry

--- src/main/java/zoniic645/firmasdelight/common/FarmersDelightItems.java
+++ src/main/java/zoniic645/firmasdelight/common/FarmersDelightItems.java
@@ -1,22 +1,9 @@
 package zoniic645.firmasdelight.common;
 
-import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraftforge.registries.RegistryObject;
-
-import java.util.Locale;
-import java.util.function.Supplier;
 
 public class FarmersDelightItems{
 	public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, "farmersdelight");
-
-	private static RegistryObject<Item> register(String name, CreativeModeTab group){
-		return register(name, () -> new Item(new Item.Properties().tab(group)));
-	}
-
-	private static <T extends Item> RegistryObject<T> register(String name, Supplier<T> item){
-		return ITEMS.register(name.toLowerCase(Locale.ROOT), item);
-	}
 }
